e2591673d8efcfb68eed1c5832c63a422eeca6f7
noteconvert <me@ashley.how>
2019-06-16 20:18:05 +0200

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,22 @@
 var languageBtn = document.getElementById("languageBtn");
-
-//let languageStoreValue = localStorage.getItem('language') === 'en' ? languageBtn.value = 'en' : languageBtn.value = 'it';
-
 var languageScript = document.getElementById("language");
 window.onload = function () {
   document.getElementById("hero").focus();
 };
 
 if (localStorage.getItem('language') === 'en') {
-  // or if this value is true, load a function that loads the italian 
   languageEN();
   languageBtn.value = 'en';
 }
 else {
   languageIT();
   languageBtn.value = 'it';
-
 }
-
-
 
 var typed = new Typed('#typed', {
   stringsElement: '#typed-strings',
   typeSpeed: 50
 });
-
-
-
 
 function changeLanguage() {
   if (languageBtn.value === 'en') {
@@ -34,16 +24,13 @@ function changeLanguage() {
     localStorage.setItem('language', languageBtn.value);
     languageScript.src = 'scripts/italiano.js'
     languageBtn.innerHTML = 'English';
-    // and refresh page
-     window.location.reload();
-
+    window.location.reload();
   }
   else {
     languageBtn.value = 'en';
     localStorage.setItem('language', languageBtn.value);
     languageScript.src = 'scripts/english.js'
     languageBtn.innerHTML = 'Italiano';
-    // and refresh page
-     window.location.reload();
+    window.location.reload();
   }
 }
--- scripts/language.js
+++ scripts/language.js
@@ -4,7 +4,6 @@ var languageScript = document.getElementById("language");
 var typed1 = document.getElementById("typed1");
 var typed2 = document.getElementById("typed2");
 var typed3 = document.getElementById("typed3");
-var typed4 = document.getElementById("typed4");
 
 var akinDesc = document.getElementById("a-kin-desc");
 
@@ -12,24 +11,22 @@ var sect2 = document.getElementById("sect2");
 
 function languageEN() {
     languageBtn.innerHTML = `Italiano`
-    typed1.innerHTML = `Hi.`
-    typed2.innerHTML = `I'm Ashley.`
-    typed3.innerHTML = `I'm a front-end web developer, based in Trieste.`
-    typed4.innerHTML = `Why not check out my projects below?`
+    typed1.innerHTML = `Hi, I'm Ashley.`
+    typed2.innerHTML = `I'm a front-end web developer, based in Trieste.`
+    typed3.innerHTML = `Why not check out my projects below?`
 
     akinDesc.innerHTML = `Musician`
 
-    sect2.innerHTML = `Section 2`
+    sect2.innerHTML = `Services`
 }
 
 function languageIT() {
     languageBtn.innerHTML = `English`
-    typed1.innerHTML = `Ciao.`
-    typed2.innerHTML = `Sono Ashley.`
-    typed3.innerHTML = `Lavoro come web developer "front-end" a Trieste.`
-    typed4.innerHTML = `Dare uno sguardo ai progetti sotto!`
+    typed1.innerHTML = `Ciao, sono Ashley.`
+    typed2.innerHTML = `Lavoro come web developer "front-end" a Trieste.`
+    typed3.innerHTML = `Dare uno sguardo ai progetti sotto!`
 
     akinDesc.innerHTML = `Musicista`
 
-    sect2.innerHTML = `Parte due`
+    sect2.innerHTML = `Servizi`
 }
