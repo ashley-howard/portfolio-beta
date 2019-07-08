71060b6381189a93deb2dfccfdd324cf81ceb8fc
noteconvert <me@ashley.how>
2019-07-08 16:18:47 +0200

    add job title and location

--- scripts/language.js
+++ scripts/language.js
@@ -5,11 +5,14 @@ var typed1 = document.getElementById("typed1");
 var typed2 = document.getElementById("typed2");
 var typed3 = document.getElementById("typed3");
 
+var area = document.getElementById("area");
+
 var scrollProjects = document.getElementById("scroll-projects");
 
 var akinDesc = document.getElementById("a-kin-desc");
 
 var sect2 = document.getElementById("sect2");
+
 
 function languageEN() {
     // Home
@@ -20,15 +23,17 @@ function languageEN() {
     scrollProjects.innerHTML = `Projects`
     akinDesc.innerHTML = `Musician`
     sect2.innerHTML = `Services`
+    area.innerHTML = `<i class="fas fa-map-marker-alt"></i> Italy`
 }
 
 function languageIT() {
     // Home
     languageBtn.innerHTML = `English`
-    typed1.innerHTML = `Ciao, sono Ashley.`
-    typed2.innerHTML = `Lavoro come web developer "front-end" a Trieste.`
+    typed1.innerHTML = `Ciao!`
+    typed2.innerHTML = `Sono Ash.`
     typed3.innerHTML = `Dare uno sguardo ai progetti sotto!`
     scrollProjects.innerHTML = `Progetti`
     akinDesc.innerHTML = `Musicista`
     sect2.innerHTML = `Servizi`
+    area.innerHTML = `<i class="fas fa-map-marker-alt"></i> Italia`
 }
