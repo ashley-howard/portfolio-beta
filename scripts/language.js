33795235721fa71a55f446927b6ba9d8a55ce064
noteconvert <me@ashley.how>
2019-08-07 09:22:36 +0200

    add contact

--- scripts/language.js
+++ scripts/language.js
@@ -1,39 +1,49 @@
 var languageBtn = document.getElementById("languageBtn");
 var languageScript = document.getElementById("language");
-
 var typed1 = document.getElementById("typed1");
 var typed2 = document.getElementById("typed2");
 var typed3 = document.getElementById("typed3");
-
 var area = document.getElementById("area");
-
 var scrollProjects = document.getElementById("scroll-projects");
-
 var akinDesc = document.getElementById("a-kin-desc");
-
 var sect2 = document.getElementById("sect2");
+var linkProj = document.getElementById("link-proj");
+var linkAbout = document.getElementById("link-about");
+var linkContact = document.getElementById("link-contact");
 
 
 function languageEN() {
-    // Home
+    // Button
     languageBtn.innerHTML = `Italiano`
+    // Menu
+    linkProj.innerHTML = `Projects`
+    linkAbout.innerHTML = `About me`
+    linkContact.innerHTML = `Contact`
+    // Hero
+    area.innerHTML = `<i class="fas fa-map-marker-alt"></i> Italy`
     typed1.innerHTML = `Hi there!`
     typed2.innerHTML = `I'm Ash.`
     typed3.innerHTML = `Why not check out some of my projects below?`
+    // Projects
     scrollProjects.innerHTML = `Projects`
     akinDesc.innerHTML = `Musician`
-    sect2.innerHTML = `Services`
-    area.innerHTML = `<i class="fas fa-map-marker-alt"></i> Italy`
+    sect2.innerHTML = `Approach`
 }
 
 function languageIT() {
-    // Home
+    // Button
     languageBtn.innerHTML = `English`
+    // Menu
+    linkProj.innerHTML = `Progetti`
+    linkAbout.innerHTML = `Su di me`
+    linkContact.innerHTML = `Contatti`
+    // Hero
+    area.innerHTML = `<i class="fas fa-map-marker-alt"></i> Italia`
     typed1.innerHTML = `Ciao!`
     typed2.innerHTML = `Sono Ash.`
-    typed3.innerHTML = `Dare uno sguardo ai progetti sotto!`
+    typed3.innerHTML = `Dai uno sguardo ai progetti qui sotto!`
+    // Projects
     scrollProjects.innerHTML = `Progetti`
     akinDesc.innerHTML = `Musicista`
-    sect2.innerHTML = `Servizi`
-    area.innerHTML = `<i class="fas fa-map-marker-alt"></i> Italia`
+    sect2.innerHTML = `Il mio approccio`
 }
